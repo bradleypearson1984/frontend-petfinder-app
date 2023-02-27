cdc92fe2a6a5c5ec97288f97648aa6ca3a06fe16
Tomas Paul Cservenak <tomas.cservenak@gmail.com>
2023-02-27 11:46:05 -0700

    adds API call and displays pets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import DisplayPets from './components/DisplayPets';
+
+const API_KEY = '1gxzT4jdmq165biOL7wNCuaBLtaS7NMyZNIWgcTXLTodwgTQQ8';
+const API_SECRET = 'K5Q3ZyDtiOrnn0cBysnVbHjFmatf42GWFupoAhQv';
+const AUTH_ENDPOINT = 'https://api.petfinder.com/v2/oauth2/token';
+const API_ENDPOINT = 'https://api.petfinder.com/v2';
 
 function App() {
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(() => {
+    let token;
+    axios.post(AUTH_ENDPOINT, {
+      grant_type: 'client_credentials',
+      client_id: API_KEY,
+      client_secret: API_SECRET
+    })
+      .then(response => {
+        token = response.data.access_token;
+        return axios.get(`${API_ENDPOINT}/animals?type=dog&page=1`, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+      })
+      .then(response => {
+        setAnimals(response.data.animals.slice(0, 9));
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DisplayPets animals={animals} />
     </div>
   );
 }
