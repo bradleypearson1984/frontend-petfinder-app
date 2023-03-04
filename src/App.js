2ec111697cd509be5480e8108442b5d5835cc20d
Tomas Paul Cservenak <tomas.cservenak@gmail.com>
2023-03-04 15:16:29 -0700

    adds Main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import CallAPI from './components/CallAPI';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
-      <CallAPI />
+      <Main />
     </div>
   );
 }
