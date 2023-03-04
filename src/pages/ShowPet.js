2335ad426713646c77ab1343594bf4813c72aac2
Zack Sigel <zack.sigel@gmail.com>
2023-03-04 17:04:26 -0500

    updates to show page

--- src/pages/ShowPet.js
+++ src/pages/ShowPet.js
@@ -11,7 +11,36 @@ import { useState, useEffect } from 'react';
 const ShowPet = (props) => {
   const navigate = useNavigate();
   const {id} = useParams();
-  
+  const pets = props.animals;
+  const pet = pet ? pet.find((p) => p._id === id) : null;
+
+  const [comment, addComment] = useState({
+    comment: "",
+  });
+
+  // comment feature needs option to edit and delete comment too
+
+  const handleChange = (event) => {
+    addComment((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    props.updatePetComment(comment, pet._id);
+  };
+
+  const handleDeleteComment = () => {
+    props.deletePetComment(pet._id);
+    navigate("/");
+  };
+
+  const handleDeletePet = () => {
+    props.deletePet(pet._id);
+    navigate("/");
+  };
 
 
 
